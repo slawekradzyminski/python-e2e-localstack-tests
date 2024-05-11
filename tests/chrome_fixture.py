9b51f7e2782421433c8723c3ae5d6a8d6529cdae
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-11 12:46:12 +0200

    parallel

--- tests/chrome_fixture.py
+++ tests/chrome_fixture.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def chrome_browser():
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     service = Service(ChromeDriverManager().install())
     browser = Chrome(service=service, options=options)
     yield browser
