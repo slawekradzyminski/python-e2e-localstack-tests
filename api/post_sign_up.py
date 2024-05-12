64904ac4bc621ca2aba1a87a9b8569ba52765e24
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-12 09:15:09 +0200

    empty branch

--- api/post_sign_up.py
+++
@@ -1,9 +0,0 @@
-from api.data.register import RegisterRequestDto
-from api.base_api import BaseAPI
-
-class SignUp(BaseAPI):
-    def api_call(self, user: RegisterRequestDto):
-        user_data = user.to_dict()
-        response = self.make_request("POST", "users/signup", json=user_data)
-        assert response.status_code == 201, "Failed to register"
-        return response.json()
