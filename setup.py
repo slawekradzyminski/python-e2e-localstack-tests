c8cbe0e3b2e3a0b6c7c0532090799b8a168304c3
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-26 20:39:48 +0200

    setup py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-e2e-localstack-tests',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'attrs==23.2.0',
+        'certifi==2024.2.2',
+        'charset-normalizer==3.3.2',
+        'h11==0.14.0',
+        'idna==3.7',
+        'iniconfig==2.0.0',
+        'outcome==1.3.0.post0',
+        'packaging==24.0',
+        'pluggy==1.5.0',
+        'PySocks==1.7.1',
+        'pytest==8.1.1',
+        'python-dotenv==1.0.1',
+        'requests==2.31.0',
+        'selenium==4.20.0',
+        'sniffio==1.3.1',
+        'sortedcontainers==2.4.0',
+        'trio==0.25.0',
+        'trio-websocket==0.11.1',
+        'urllib3==2.2.1',
+        'webdriver-manager==4.0.1',
+        'wsproto==1.2.0',
+        'pytest-xdist==3.6.1',
+        'Faker==25.1.0',
+        'allure-pytest==2.13.5'
+    ],
+    python_requires='>=3.12.1',
+)
